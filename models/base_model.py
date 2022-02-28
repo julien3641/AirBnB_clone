2e561259a24bb859a6945b3dcad7ba96e68508a5
Anthony-6 <anthony.lacoste5@gmail.com>
2022-02-28 15:50:08 +0100

    update basemodel with instruction of task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,18 @@ class BaseModel:
     """
     This class defines all common attributes/methods for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         This is the constructor method.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(args) == 0 and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
