124e0b5f0da920e57fde1b8c2fb4480e11eb2e3d
Anthony-6 <anthony.lacoste5@gmail.com>
2022-03-02 10:25:12 +0100

    update file console with do_all command created by elodie

--- console.py
+++ console.py
@@ -82,20 +82,21 @@ class HBNBCommand(cmd.Cmd):
             del models.storage.all()[key]
             models.storage.save()
 
+    def do_update(self, line):
+        my_list = line.split(' ')
+
     def do_all(self, line):
         """all command to print all string representation of all instance
         """
-        if line == "":
+        my_list = line.split(' ')
+        if line == "" or my_list[0] in self.list_class:
             my_list_str = []
             my_str = models.storage.all().items()
             for key, value in my_str:
                 my_list_str.append(str(value))
             print("{}".format(my_list_str))
-        else:
-            my_list = line.split(' ')
-            if my_list[0] not in self.list_class:
-                print("** class doesn't exist **")
-
+        elif my_list[0] not in self.list_class:
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
